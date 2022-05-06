7079f86dc5257a37b8a3a6b60324f9b5a49d06f3
Adam <235885@edu.p.lodz.pl>
2022-05-06 11:45:12 +0200

    all tests passed, added tests for other classes

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -1,5 +1,6 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.list.IntLinkedList;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -19,12 +20,12 @@ public class RhymersJUnitTest {
 	public void testCallCheck() {
 		DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
 		boolean result = rhymer.callCheck();
-		Assert.assertEquals(true, result);
+		Assert.assertTrue(result);
 
 		rhymer.countIn(888);
 
 		result = rhymer.callCheck();
-		Assert.assertEquals(false, result);
+		Assert.assertFalse(result);
 	}
 
 	@Test
@@ -33,12 +34,12 @@ public class RhymersJUnitTest {
 		final int STACK_CAPACITY = 12;
 		for (int i = 0; i < STACK_CAPACITY; i++) {
 			boolean result = rhymer.isFull();
-			Assert.assertEquals(false, result);
+			Assert.assertFalse(false);
 			rhymer.countIn(888);
 		}
 
 		boolean result = rhymer.isFull();
-		Assert.assertEquals(true, result);
+		Assert.assertTrue(true);
 	}
 
 	@Test
@@ -75,4 +76,46 @@ public class RhymersJUnitTest {
 		Assert.assertEquals(EMPTY_STACK_VALUE, result);
 	}
 
+	@Test
+	public void testIntLinkedList(){
+		IntLinkedList linkedList = new IntLinkedList();
+		boolean result =  linkedList.isEmpty();
+		Assert.assertTrue(result);
+		linkedList.push(0);
+		Assert.assertEquals(linkedList.pop(),0);
+		result = linkedList.isEmpty();
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void testFIFORhymer(){
+		FIFORhymer fifoRhymer = new FIFORhymer();
+		Assert.assertEquals(fifoRhymer.countOut(), -1);
+		fifoRhymer.countIn(10);
+		Assert.assertEquals(fifoRhymer.countOut(),10);
+		Assert.assertTrue(fifoRhymer.callCheck());
+	}
+
+	@Test
+	public void testHanoiRhymer(){
+		HanoiRhymer hanoiRhymer = new HanoiRhymer();
+		Assert.assertEquals(hanoiRhymer.reportRejected(),0);
+
+		hanoiRhymer.countIn(10);
+		hanoiRhymer.countIn(11);
+		Assert.assertEquals(hanoiRhymer.reportRejected(),1);
+		Assert.assertEquals(hanoiRhymer.peekaboo(), 10);
+
+		for(int i = 10;i<15;i++){
+			hanoiRhymer.countIn(i);
+		}
+		Assert.assertEquals(hanoiRhymer.reportRejected(), 5);
+
+		for(int i = 9;i>=0;i--){
+			hanoiRhymer.countIn(i);
+		}
+		Assert.assertEquals(hanoiRhymer.getTotal(),11);
+		Assert.assertEquals(hanoiRhymer.reportRejected(), 5);
+		Assert.assertTrue(hanoiRhymer.isFull());
+	}
 }
